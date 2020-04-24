86041567069ba216169d0abed9cab118994b501a
dcep93 <dan@quizlet.com>
2020-04-24 12:21:42 -0700

    safer determine time

--- extension/inject_netflix.js
+++ extension/inject_netflix.js
@@ -31,7 +31,7 @@
 	document.head.appendChild(injectedElement);
 
 	function determineTime(state) {
-		if (state.paused) return state.time;
+		if (state.paused || !state.speed) return state.time;
 		var ageMs = Date.now() - parseInt(state.date);
 		var secondsAdvanced = (ageMs * parseFloat(state.speed)) / 1000;
 		return parseFloat(state.time) + secondsAdvanced;
