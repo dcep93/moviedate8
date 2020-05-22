2668d9553bcdca77b4fc82bf6307730f92bde4f6
dcep93 <dan@quizlet.com>
2020-05-22 16:21:11 -0700

    worse before better

--- widget/widget.js
+++ widget/widget.js
@@ -2,17 +2,23 @@ const id = Math.random().toFixed(4).slice(2);
 const handleInject = () => null;
 const inject = null;
 
+var src = location.href.split("src=")[1];
+const videoE = document.getElementById("video");
+if (src) videoE.src = decodeURIComponent(src);
+
 var listener;
 
-function query(_, f) {
-	window.onload = () => src && f([{ id }]);
+function queryW(_, f) {
+	window.onload = () => {
+		src && f([{ id }]);
+	};
 }
 
 function getProfileUserInfo(f) {
 	f({ email: "widget" });
 }
 
-function sendMessage(_, payload, f) {
+function sendMessageW(_, payload, f) {
 	listener(payload, null, f);
 }
 
@@ -20,8 +26,15 @@ function addListener(f) {
 	listener = f;
 }
 
+const fromFileInput = document.getElementById("from_file");
+fromFileInput.addEventListener("change", () => {
+	src = fromFileInput.files[0];
+	if (src) videoE.src = URL.createObjectURL(src);
+	videoE.oncanplay = window.onload;
+});
+
 window.chrome = {
-	tabs: { query, sendMessage },
+	tabs: { query: queryW, sendMessage: sendMessageW },
 	identity: { getProfileUserInfo },
 	runtime: { onMessage: { addListener } },
 };
