37760c5e6ee5c85060214538339f860f915b6719
dcep93 <dan@quizlet.com>
2022-07-01 11:34:25 -0700

    info

--- app/moviedate8/src/moviedate8/Info.tsx
+++ app/moviedate8/src/moviedate8/Info.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { StateEnum, WatchersType, WatcherType } from "./firebase";
+import css from "./index.module.css";
 
 const RENDER_INTERVAL_MS = 10;
 
@@ -17,12 +18,14 @@ class Info extends React.Component<{ leader: string; watchers: WatchersType }> {
       .sort((a, b) => (a.user_name < b.user_name ? -1 : 1));
     return (
       <div>
-        <div>
-          <SubInfo w={leaderW} />
-        </div>
+        {leaderW && (
+          <div>
+            <SubInfo w={leaderW} />
+          </div>
+        )}
         <div>
           {sortedNonLeaders.map((w) => (
-            <SubInfo key={w.user_name} w={w} />
+            <SubInfo key={w.user_name} w={w} url={leaderW?.url} />
           ))}
         </div>
       </div>
@@ -30,15 +33,24 @@ class Info extends React.Component<{ leader: string; watchers: WatchersType }> {
   }
 }
 
-function SubInfo(props: { w: WatcherType }) {
+function SubInfo(props: { w: WatcherType; url?: string }) {
   const progress =
     props.w.progress +
     ((Date.now() - props.w.timestamp) *
       (props.w.state === StateEnum.playing ? props.w.speed : 0)) /
       1000;
   return (
-    <div>
-      {props.w.user_name} {progress.toFixed(2)} {props.w.speed}
+    <div
+      title={JSON.stringify(props, null, 2)}
+      className={[
+        css.subinfo,
+        props.url && props.w.url !== props.url && css.brokensubinfo,
+      ].join(" ")}
+    >
+      <div>
+        {props.w.user_name} {props.w.speed.toFixed(2)}x
+      </div>
+      <div>{progress.toFixed(2)}</div>
     </div>
   );
 }
