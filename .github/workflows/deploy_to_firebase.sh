bba97190da9984d3afd8a7486b956e5f7405cdb9
dcep93 <dan@quizlet.com>
2022-06-30 19:01:08 -0700

    x

--- .github/workflows/deploy_to_firebase.sh
+++ .github/workflows/deploy_to_firebase.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 
 SA_KEY="$1"
 
-# GOOGLE_CLOUD_PROJECT=moviedate8
 # firebase init hosting --project "$GOOGLE_CLOUD_PROJECT"
 # gcloud iam service-accounts create deployer-github
 # gcloud projects add-iam-policy-binding "$GOOGLE_CLOUD_PROJECT" --member="serviceAccount:deployer-github@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com" --role="roles/firebasehosting.admin"
@@ -12,8 +11,33 @@ SA_KEY="$1"
 # cat gac.json
 
 cd app/moviedate
+
 export GOOGLE_APPLICATION_CREDENTIALS="gac.json"
 echo "$SA_KEY" > "$GOOGLE_APPLICATION_CREDENTIALS"
 npm install -g firebase-tools
 gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"
-firebase deploy --project "$(cat $GOOGLE_APPLICATION_CREDENTIALS | jq -r .project_id)"
+project_id="$(cat $GOOGLE_APPLICATION_CREDENTIALS | jq -r .project_id)"
+
+
+cat <<EOF > firebase.json
+{
+    "hosting": {
+        "public": "build",
+        "ignore": ["firebase.json", "**/.*", "**/node_modules/**"],
+        "rewrites": [{
+            "source": "**",
+            "destination": "/index.html"
+        }]
+    }
+}
+EOF
+
+cat <<EOF > .firebaserc
+{
+    "projects": {
+        "default": "$project_id"
+    }
+}
+EOF
+
+firebase deploy --project "$project_id"
