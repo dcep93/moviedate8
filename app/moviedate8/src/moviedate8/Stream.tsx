d75e0c83f93dc777941180b8c6a2f06cc3bd3c0b
dcep93 <dcep93@gmail.com>
2024-06-18 21:19:37 -0700

    x

--- app/moviedate8/src/moviedate8/Stream.tsx
+++ app/moviedate8/src/moviedate8/Stream.tsx
@@ -4,7 +4,17 @@ import Selector from "./Selector";
 import { FirebaseWrapper, LibraryType } from "./firebase";
 import css from "./index.module.css";
 
-class Stream extends FirebaseWrapper<
+function Stream() {
+  return (
+    <video
+      controls
+      onError={(e) => alert(`error: ${JSON.stringify(e)}`)}
+      src="https://rs16.seedr.cc/ff_get_premium/5450367353/Talk%20to%20Me%20(2023)%20(1080p%20BluRay%20x265%2010bit).mp4?st=S0Yo3k8zy7pWPbkNrQVAiQ&e=1718943560"
+    />
+  );
+}
+
+class StreamX extends FirebaseWrapper<
   LibraryType | undefined,
   { rawToStream?: string }
 > {
