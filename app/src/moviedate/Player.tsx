e57c303a1e22743bad7e02eac98e56e9967c37ed
Daniel Cepeda <dcep93@gmail.com>
2026-01-17 03:09:27 -0500

    x

--- app/src/moviedate/Player.tsx
+++ app/src/moviedate/Player.tsx
@@ -2,7 +2,7 @@ export default function Player({ src }: { src: string }) {
   return (
     <div>
       <div style={{ width: "100vW", height: "100vH" }}>
-        <video src={src} />
+        <video src={src} controls />
       </div>
     </div>
   );
