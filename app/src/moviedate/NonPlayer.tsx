ef785fbaf98c9b8fa8d55b427e61eb4845c5f68e
Daniel Cepeda <dcep93@gmail.com>
2026-01-17 04:28:00 -0500

    x

--- app/src/moviedate/NonPlayer.tsx
+++ app/src/moviedate/NonPlayer.tsx
@@ -4,6 +4,8 @@ import _firebase from "./_firebase";
 import type { PlayerConfig } from "./Player";
 import { rootPath } from "./Root";
 
+const K_QUERY_PARAM = "k";
+
 export type Data = {
   library: { [key: string]: string };
 } | null;
@@ -24,27 +26,41 @@ export default function NonPlayer({
       _firebase._set(libraryPath(path), src);
     }
   }
-  const [searchParams] = useSearchParams();
-  const key = searchParams.get("key");
-  console.log({ key });
+  const [searchParams, setSearchParams] = useSearchParams();
+  const key = searchParams.get(K_QUERY_PARAM);
+  const src = data?.library?.[key ?? ""];
   useEffect(() => {
-    if (!key) return;
-    const src = data?.library?.[key];
     if (!src) return;
     setPlayerConfig({ src });
-  }, [data?.library, key, setPlayerConfig]);
+  }, [data?.library, src, setPlayerConfig]);
+  useEffect(() => {
+    if (!key) return;
+    if (!pathInputRef.current) return;
+    pathInputRef.current!.value = key;
+    if (!src) return;
+    if (!srcInputRef.current) return;
+    srcInputRef.current!.value = src;
+  }, [pathInputRef, srcInputRef, src]);
   return (
     <div>
-      <ul>
-        {Object.keys(data?.library ?? []).map((key) => (
-          <li key={key}>
-            <button onClick={() => _firebase._set(libraryPath(key), null)}>
-              ❌
-            </button>
-            <button onClick={() => searchParams.set("key", key)}>{key}</button>
-          </li>
-        ))}
-      </ul>
+      {!data ? null : (
+        <ul>
+          {Object.keys(data.library ?? []).map((key) => (
+            <li key={key}>
+              <button onClick={() => _firebase._set(libraryPath(key), null)}>
+                ❌
+              </button>
+              <button
+                onClick={() =>
+                  setSearchParams((p) => (p.set(K_QUERY_PARAM, key), p))
+                }
+              >
+                {key}
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
       <div>
         <input ref={pathInputRef} onSubmit={submit} />
         <input ref={srcInputRef} onSubmit={submit} />
