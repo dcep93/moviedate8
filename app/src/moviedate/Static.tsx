1b24e0da6361bbf9e01d829f3e9c4be8672cbab5
Daniel Cepeda <dcep93@gmail.com>
2026-01-17 03:07:42 -0500

    x

--- app/src/moviedate/Static.tsx
+++ app/src/moviedate/Static.tsx
@@ -1,6 +1,6 @@
 import Player from "./Player";
 
-import static_path from "./static_path.txt";
+import static_path from "./static_path.txt?raw";
 
 export default function Static() {
   return <Player src={static_path} />;
