888e8a449adaa49646bb6f116b569e4eb5525878
Daniel Cepeda <dcep93@gmail.com>
2026-01-17 02:34:01 -0500

    x

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Moviedate from "./moviedate/index.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Moviedate />
   </StrictMode>,
-)
+);
